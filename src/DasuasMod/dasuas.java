b2308a114ca049a364b75f2e65fb243805dd8198
mrybs <mrybs@github.com>
2022-11-05 15:52:47 +0300

    Java disabled :( (0.1-p)

--- src/DasuasMod/dasuas.java
+++ src/DasuasMod/dasuas.java
@@ -1,11 +1,8 @@
-package dasuas;
+package DasuasMod;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
